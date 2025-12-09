4c8ae2463a4e3b0ed077c1042d05761ed03d09a3
MrPikachy <bondar.nazar120@gmail.com>
2025-12-09 12:50:30 +0200

    Change documentation route to '/docs'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/docs')
 def documentation():
     return """
     <h1>LPNU Schedule API</h1>
@@ -105,4 +105,5 @@ def get_schedule():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
